33699b931929e1379c024e26b9905940c6583b88
Abdullah <abdullahan1928@gmail.com>
2023-05-05 13:32:58 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+})
